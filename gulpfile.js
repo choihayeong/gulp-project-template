eadb25d10327e2867fdfa9e3a70935d720013c7e
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-04-18 17:38:37 +0900

    stylesheet, del, browser-sync 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,56 @@
-const { src, dest, parallel, series } = require("gulp");
+const { src, dest, parallel, series, watch } = require("gulp");
 const ejs = require("gulp-ejs");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass")(require("sass"));
 
+const ROOT = "/";
+const PORT = 8080;
+
+const ASSET_PATH = {
+    HTML: {
+        src: "./src/views",
+        dest: "./output/"
+    },
+    SCSS: {
+        src: "./src/scss/common.scss",
+        dest: "./output/css/",
+        watch: "./src/scss/**/*.scss",
+    }
+};
+
+// markup
 const gulpEJS = () => 
-    src(["./src/views/**/*.html", '!'+"./src/views" + "/**/include/*.html"])
+    src([ASSET_PATH.HTML.src+"/**/*.html", '!'+ASSET_PATH.HTML.src + "/**/include/*.html"])
     .pipe(ejs())
-    .pipe(dest("output/"));
+    .pipe(dest(ASSET_PATH.HTML.dest));
 
+// stylesheets
+const stylesheets = () => 
+    src(ASSET_PATH.SCSS.src)
+    .pipe(sass({
+        outputStyle: "compressed"
+    }).on("error", sass.logError))
+    .pipe(dest(ASSET_PATH.SCSS.dest));
 
-exports.default = parallel([gulpEJS]);
+// browser-sync 
+const bs = () => {
+    browserSync.init({
+        server: {
+            baseDir: __dirname + ROOT,
+            directory: true,
+        },
+        port : PORT,
+        open: false,
+    });
+};
+    
+const watching = () => {
+    watch(ASSET_PATH.HTML.src+"/**/*.html", gulpEJS).on("change", browserSync.reload);
+    watch(ASSET_PATH.SCSS.watch, stylesheets).on("change", browserSync.reload);
+};
+
+const clean = () => del(["output"]);
+
+exports.default = parallel([gulpEJS, stylesheets, watching, bs]);
+exports.clean = series(clean);
