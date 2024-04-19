6ec0aef24a56bc6a7a92fd7d38d6228e353c8762
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-04-19 15:54:08 +0900

    wip : gulp-inline-source 적용

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass")(require("sass"));
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
+const imagemin = require("gulp-imagemin");
+const inlineSource = require("gulp-inline-source");
 
 const ROOT = "/";
 const PORT = 8080;
@@ -23,37 +25,79 @@ const ASSET_PATH = {
         src: "./src/scripts/common.js",
         dest: "./output/scripts/bundle.js",
         watch: "./src/scripts/**/*.js",
+    },
+    IMAGE: {
+        src: "./src/images/*",
+        dest: "./output/images/",
     }
 };
 
-// markup
-const gulpEJS = () => 
+/**
+ * @task : Templates
+ */
+const gulpEJS = () => new Promise(resolve => {
     src([ASSET_PATH.HTML.src+"/**/*.html", '!'+ASSET_PATH.HTML.src + "/**/include/*.html"])
     .pipe(ejs())
     .pipe(dest(ASSET_PATH.HTML.dest));
 
-// stylesheets
-const stylesheets = () => 
+    resolve();
+});
+
+/**
+ * @task : Sass 
+ */
+const stylesheets = () => new Promise(resolve => {
     src(ASSET_PATH.SCSS.src)
-    .pipe(sass({
-        outputStyle: "compressed"
-    }).on("error", sass.logError))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(dest(ASSET_PATH.SCSS.dest));
 
-// scripts
-const getBrowserScript = () => {
+    resolve();
+});
+
+/**
+ * @task : scripts 
+ */
+const getBrowserScript = () => new Promise(resolve => {
     browserify({
         basedir: ".",
         debug: true,
         entries: [ASSET_PATH.SCRIPT.src],
         cache: {},
         packageCache: {},
+    }).transform("babelify", {
+        sourceMapsAbsolute: false,
+        presets: ["@babel/preset-env"]
     }).bundle()
     .pipe(source(ASSET_PATH.SCRIPT.dest))
-    .pipe(dest("output"));
-};
+    .pipe(dest("./"));
 
-// browser-sync 
+    resolve();
+});
+
+/**
+ * @task : Imagemin 
+ */
+const images = () => new Promise(resolve => {
+    src(ASSET_PATH.IMAGE.src)
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: false}),
+            imagemin.mozjpeg({quality: 95, progressive: false}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({plugins: [{removeViewBox: true}, {cleanupIDs: false}]})
+        ]))
+        .pipe(dest(ASSET_PATH.IMAGE.dest));
+
+    resolve();
+});
+
+const inlineResource = () =>
+    src("./src/**/*.html")
+        .pipe(inlineSource())
+        .pipe(dest("./output"));
+
+/**
+ * @task : browserSync
+ */
 const bs = () => {
     browserSync.init({
         server: {
@@ -64,14 +108,32 @@ const bs = () => {
         open: false,
     });
 };
-    
+
+/**
+ * @task : Watch
+ */
 const watching = () => {
     watch(ASSET_PATH.HTML.src+"/**/*.html", gulpEJS).on("change", browserSync.reload);
     watch(ASSET_PATH.SCSS.watch, stylesheets).on("change", browserSync.reload);
     watch(ASSET_PATH.SCRIPT.watch, getBrowserScript).on("change", browserSync.reload);
 };
 
-const clean = () => del(["output"]);
 
-exports.default = parallel([gulpEJS, stylesheets, getBrowserScript, watching, bs]);
+/**
+ * @task : Clean 
+ */
+const clean = () => new Promise(resolve => {
+    del.sync(["output"]);
+
+    resolve();
+});
+
+const assets = parallel([gulpEJS, stylesheets, getBrowserScript, images]);
+const liveServer = parallel([watching, bs]);
+
+exports.default = parallel(assets, liveServer);
+exports.server = series(liveServer);
+exports.build = series(clean, assets);
 exports.clean = series(clean);
+
+exports.inline = series(inlineResource);
